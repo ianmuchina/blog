1ab9c84453f54380339098aca7cf84dff4450566
Ian Muchina <noreply@ianmuchina.com>
2022-06-01 10:02:44 +0300

    fix ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ "$ENVIRONMENT" == "dev" ] && export ARGS="-D"
 rm public/ -fr
-rm resources/_gen/ -fr 
-yarn install
-yarn hugo $ARGS
+rm resources/_gen/ -fr
+npm install
+"$(npm bin)"/hugo $ARGS
